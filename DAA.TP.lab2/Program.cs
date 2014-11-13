5cfb0c723b92937390bd667174ff8a4e18c4af35
QuarK264 <alexey.danilov@hotmail.com>
2014-11-13 18:17:59 +0300

    Вынес шаблон в отдельный класс, вынес проверку в отдельный метод.

--- DAA.TP.lab2/Program.cs
+++ DAA.TP.lab2/Program.cs
@@ -5,17 +5,21 @@
 
     class Program
     {
+        private static string GUIDCheck(string GUID)
+        {
+            Regex match = new Regex(Pattern.pattern, RegexOptions.IgnoreCase);
+            return match.IsMatch(GUID).ToString();
+        }
+
         static void Main()
         {
-            string pattern = @"\G([0-9a-f]+-{1})+([0-9a-f]{12})";
             string GUID = "6F9619FF-8B86-D011-B42D-00CF4FC964FF";
-            Regex match = new Regex(pattern, RegexOptions.IgnoreCase);
-            Console.WriteLine(match.IsMatch(GUID));
-            //foreach (Match GuidPart in Regex.Matches(GUID, pattern, RegexOptions.IgnoreCase))
+            Console.WriteLine(GUIDCheck(GUID));
+            //foreach (Match GuidPart in Regex.Matches(GUID, Pattern.pattern, RegexOptions.IgnoreCase))
             //{
-                //Console.WriteLine(GuidPart.Value);
+            //    Console.WriteLine(GuidPart.Value);
             //}
-            Console.WriteLine(Regex.Match(GUID, pattern, RegexOptions.IgnoreCase));
+            Console.WriteLine(Regex.Match(GUID, Pattern.pattern, RegexOptions.IgnoreCase));
             Console.ReadLine();
         }
     }
