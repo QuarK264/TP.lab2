23430b60bc2fe49ef3b3ebba2faab4d944d9996d
QuarK264 <alexey.danilov@hotmail.com>
2014-11-13 18:28:29 +0300

    Добавил остальное, чтобы проект собирался

---
+++ DAA.TP.lab2/Pattern.cs
@@ -0,0 +1,10 @@
+﻿namespace DAA.TP.lab2
+{
+    using System;
+
+
+    static class Pattern
+    {
+        public static string pattern = @"\G([0-9a-f]{8}-{1})([0-9a-f]{4}-{1}){3}([0-9a-f]{12})";
+    }
+}
