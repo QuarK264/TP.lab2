854900ff4f1dc5a46125835c6af91d4f930658e9
QuarK264 <alexey.danilov@hotmail.com>
2014-11-17 20:26:06 +0300

    Добавил изменённые на паре файлы, и всё остальное для сборки проекта.

--- DAA.TP.lab2/Pattern.cs
+++ DAA.TP.lab2/Pattern.cs
@@ -2,9 +2,14 @@
 {
     using System;
 
-
+    /// <summary>
+    /// Статичный класс, содержащий шаблон регулярного выражения
+    /// </summary>
     static class Pattern
     {
-        public static string pattern = @"\G([0-9a-f]{8}-{1})([0-9a-f]{4}-{1}){3}([0-9a-f]{12})";
+        /// <summary>
+        /// Шаблон проверяющий GUID
+        /// </summary>
+        public static string pattern = @"\G\(*([0-9a-f]{8}-{1})([0-9a-f]{4}-{1}){3}([0-9a-f]{12})\)*";
     }
 }
--- DAA.TP.lab2/Program.cs
+++ DAA.TP.lab2/Program.cs
@@ -5,21 +5,30 @@
 
     class Program
     {
+        /// <summary>
+        /// Проверка GUID на корректность с помощью регулярного выражения
+        /// </summary>
+        /// <param name="GUID">GUID, который нужно проверить</param>
+        /// <returns>Результат проверки GUID</returns>
         private static string GUIDCheck(string GUID)
         {
-            Regex match = new Regex(Pattern.pattern, RegexOptions.IgnoreCase);
-            return match.IsMatch(GUID).ToString();
+            Regex match = new Regex(Pattern.pattern, RegexOptions.IgnoreCase);//создание экземпляра класса, по шаблону с игнорированием регистра
+            return match.IsMatch(GUID).ToString();//проверка совпадает строка с шаблоном или нет
         }
-
+        /// <summary>
+        /// Точка входа в программу
+        /// </summary>
         static void Main()
         {
-            string GUID = "6F9619FF-8B86-D011-B42D-00CF4FC964FF";
-            Console.WriteLine(GUIDCheck(GUID));
+            string GUID1 = "6F9619FF-8B86-D011-B42D-00CF4FC964FF";
+            string GUID2 = "(6F9619FF-8B86-D011-B42D-00CF4FC964FF)";
+            Console.WriteLine(GUIDCheck(GUID1));
+            Console.WriteLine(GUIDCheck(GUID2));
             //foreach (Match GuidPart in Regex.Matches(GUID, Pattern.pattern, RegexOptions.IgnoreCase))
             //{
             //    Console.WriteLine(GuidPart.Value);
             //}
-            Console.WriteLine(Regex.Match(GUID, Pattern.pattern, RegexOptions.IgnoreCase));
+            //Console.WriteLine(Regex.Match(GUID, Pattern.pattern, RegexOptions.IgnoreCase));
             Console.ReadLine();
         }
     }
